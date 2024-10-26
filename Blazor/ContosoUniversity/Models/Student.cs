910087d58cc23056b347aeb726ce9d64387aa3f8
FursovArtem <131772117+FursovArtem@users.noreply.github.com>
2024-10-26 18:25:14 +0400

    university initialize students

---
+++ Blazor/ContosoUniversity/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public required string LastName { get; set; }
+        public required string FirstName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        //Navigation property
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
