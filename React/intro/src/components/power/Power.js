e9775a6a7a248af9675125b47e118c82817aece0
FursovArtem <131772117+FursovArtem@users.noreply.github.com>
2024-11-28 17:06:18 +0400

    nav menu DONE

--- React/intro/src/components/power/Power.js
+++ React/intro/src/components/power/Power.js
@@ -1,14 +1,12 @@
-function Power(props)
-{
+function Power(props) {
     let a = props.a;
     let n = props.n;
-    let N=1;
-    for(let i = 0; i<n; i++)
-    {
-        N = N*a;
+    let N = 1;
+    for (let i = 0; i < n; i++) {
+        N = N * a;
     }
 
-    return(
+    return (
         <h3>{a}<sup>{n}</sup> = {N};</h3>
     )
 }
